45ac0fafe6939711adbd0d264fff221da5649ae9
floadyun <floadyun@163.com>
2019-09-09 20:55:17 +0800

    修改UI，完善功能

--- app/src/main/java/com/iwinad/drink/activity/EndActivity.java
+++ app/src/main/java/com/iwinad/drink/activity/EndActivity.java
@@ -1,8 +1,13 @@
 package com.iwinad.drink.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import com.base.lib.util.AppManager;
 import com.iwinad.drink.R;
@@ -23,6 +28,8 @@ public class EndActivity extends AppBaseActivity {
 
     private Handler mHandler;
 
+    private int[] viewIds = new int[]{R.id.end_menu_1,R.id.end_menu_2,R.id.end_menu_3};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,5 +45,44 @@ public class EndActivity extends AppBaseActivity {
                 finishSelf();
             }
         },5000);
+        for (int i=0;i<viewIds.length;i++){
+            startViewAnimation(viewIds[i]);
+        }
+    }
+    private void startViewAnimation(int viewId){
+        View animView = findViewById(viewId);
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(animView,"ScaleX", 0.9f);
+        scaleX.setDuration(1000);
+        scaleX.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator scaleX = ObjectAnimator.ofFloat(animView,"ScaleX", 1.0f);
+                scaleX.setDuration(1000);
+                scaleX.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        startViewAnimation(viewId);
+                    }
+                });
+                scaleX.start();
+            }
+        });
+        scaleX.start();
+
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(animView,"ScaleY", 0.9f);
+        scaleY.setDuration(1000);
+        scaleY.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                ObjectAnimator scaleY = ObjectAnimator.ofFloat(animView,"ScaleY", 1.0f);
+                scaleY.setDuration(1000);
+                scaleY.start();
+            }
+        });
+        scaleY.start();
     }
 }
